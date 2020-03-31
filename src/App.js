6a79fde8d6ebef09d712ac1c2460b104c7b2306c
Joseph Miller <millerj@pixia.com>
2020-03-31 19:01:33 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import States from './components/states';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    states: []
+  }
+  componentDidMount() {
+    fetch('https://covidtracking.com/api/states')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ states: data })
+    })
+    .catch(console.log)
+  }
+  render () {
+    return (
+      <States states={this.state.states} />
+    );
+  }
 }
 
 export default App;
---
+++ src/components/states.js
@@ -0,0 +1,23 @@
+    // src/components/contacts.js
+
+    import React from 'react'
+
+    const States = ({ states }) => {
+      return (
+        <div>
+          <center><h1>Latest State Covid Figures</h1></center>
+          {states.map((state) => (
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">{state.state}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Total Tests: {state.totalTestResults}</h6>
+                <p class="card-text">Positive: {state.positive}</p>
+                <p class="card-text">Negative: {state.negative}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )
+    };
+
+    export default States
